5431867ab573b4ed2a7c538629dfa9996be97c7a
Panya Sukphranee <broncop@panyas-mbp.mynetworksettings.com>
2022-12-24 22:20:29 -0500

    explicitly stated text-balck

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="w-full flex-col">
+    <div className="w-full flex-col text-black">
       {/* <Navbar /> */}
       <Hero />
       <About />
