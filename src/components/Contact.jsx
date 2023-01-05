db0d5cb502364c43837ba314d87e19f1e359b6f7
Panya Sukphranee <psukphranee@gmail.com>
2023-01-04 19:50:07 -0800

    modified contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import Form from './Form'
 
 const Contact = () => {
+
+
+
   return (
     <div className='w-full'>
       {/* Contact */}
@@ -13,19 +17,7 @@ const Contact = () => {
         <div className='flex justify-center border-dashed'>
           {/* form */}
           <div className="w-full p-2 max-w-2xl">
-            <form method="POST" action="/send-message">
-              <div className='flex flex-col p-2'>
-                <label htmlFor="name" className="my-2">Name:</label>
-                <input type="text" id="name" name="name" className="outline" />
-                <label htmlFor="email" className="my-2">Email:</label>
-                <input type="email" id="email" name="email" className="outline" />
-                <label htmlFor="message" className="my-2">Message:</label>
-                <textarea id="message" name="message" className="p-2 outline" />
-                <div className='w-full flex justify-center p-2 m-2'>
-                  <button type="submit" className="btn-primary m-2 outline">Send Message</button>
-                </div>
-              </div>
-            </form>
+            <Form />
           </div>
         </div>
       </section>
