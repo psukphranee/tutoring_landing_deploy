ea48da443b2bb9ca97c46ce81f85e3746adeae02
Panya Sukphranee <broncop@Panyas-MBP.attlocal.net>
2023-01-17 21:37:49 -0800

    added images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import hero_image from "../assets/image_1.jpg"
+import hero_image from "../assets/manhattan_library_1.jpg"
 
 const Hero = () => {
   return (
     <div className="rounded-lg w-full my-2 p-2 bg-blue-100">
         {/* Introduction */}
-        <div className=''>
+        <div className='hero'>
             <header className="text-center py-16 px-4 mx-4 my-8">
                 <h1 className="text-4xl font-bold my-2">Math Tutoring Services</h1>
                 <p className="text-xl font-semibold">Expert help with math homework, exams, and more</p>
