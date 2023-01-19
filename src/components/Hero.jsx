8242c76c23a10839b8fb201bd65def7210229847
Panya Sukphranee <broncop@Panyas-MacBook-Pro.local>
2023-01-18 19:29:24 -0800

    removed hero property

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="rounded-lg w-full bg-black">
         {/* Introduction */}
-        <div className='hero bg-cover flex h-screen'>
+        <div className='bg-cover flex h-screen'>
             <header className="text-center py-16 px-4 m-auto">
                 <h1 className="text-4xl text-white font-bold my-2">Math Tutoring Service</h1>
                 <p className="text-xl text-white text-shadow-md font-semibold">Expert help with math homework, exams, and more</p>
