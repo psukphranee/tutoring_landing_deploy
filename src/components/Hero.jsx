7a9bc5a8881d13ba7a25edf84f47298278dd267e
Panya Sukphranee <broncop@Panyas-MacBook-Pro.local>
2023-01-18 19:12:44 -0800

    resolved background url

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import hero_image from "../assets/manhattan_library_1.jpg"
+// import hero_image from "../assets/manhattan_library_1.jpg"
 
 const Hero = () => {
   return (
-    <div className="rounded-lg w-full bg-blue-100">
+    <div className="rounded-lg w-full bg-black">
         {/* Introduction */}
         <div className='hero bg-cover flex h-screen'>
             <header className="text-center py-16 px-4 m-auto">
-                <h1 className="text-4xl text-white font-bold my-2">Math Tutoring Services</h1>
+                <h1 className="text-4xl text-white font-bold my-2">Math Tutoring Service</h1>
                 <p className="text-xl text-white text-shadow-md font-semibold">Expert help with math homework, exams, and more</p>
             </header>
         </div>
