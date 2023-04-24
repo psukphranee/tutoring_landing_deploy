b603dd8ded402251f6266f02a02e3cc7becedc72
Panya Sukphranee <psukphranee@gmail.com>
2023-04-24 00:52:37 -0700

    got snap scroll to work. the syntax given by chatgpt was off.

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -9,10 +9,9 @@ export default function Location() {
   }
 
     return (
-      <section className="my-10 w-screen" id="Location">
-
-        <div className="flex flex-col bg-gray-900 mt-8 py-24 sm:py-32">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <section className="my-6 w-screen" id="Location">
+        <div className="h-screen flex flex-col bg-gray-900 py-2 sm:py-32">
+          <div className="overflow-y-scroll my-2 mx-auto max-w-7xl px-6 lg:px-8">
             <div className="grid grid-cols-1 items-center gap-x-8 gap-y-16 lg:grid-cols-2">
               <div className="mx-auto w-full max-w-xl lg:mx-0">
                 <h2 className="text-3xl font-bold tracking-tight text-white">Online Tutoring, Teaching, and Consultations</h2>
@@ -20,17 +19,7 @@ export default function Location() {
                   With online tutoring and consultations, you can access quality educational resources from the comfort of your own home. Using online platforms such as Google Meet and Zoom and the built-in whiteboard features, enjoy engaging and interactive sessions tailored to your individual learning style and goals. Choose from a variety of platforms such as Khan Academy. 
                 </p>
                 
-                <div className="mt-8 flex items-center gap-x-6">
-                  <button
-                    onClick={scrollToServices}
-                    className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                  >
-                    Contact Me
-                  </button>
-                  
-                </div>
               </div>
-
               {/* <div className="mx-auto grid w-full max-w-xl grid-cols-2 items-center gap-y-12 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:pl-8">
                 <img
                   className="max-h-10 w-full object-contain object-left"
@@ -45,7 +34,6 @@ export default function Location() {
                   alt="Reform"
                 />
               </div> */}
-
             </div>
           </div>
           <div className="flex item-center justify-center py-6">
--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -20,38 +20,51 @@ export default function TestComponent() {
   };
 
   return (
-    <div className="relative w-full overflow-hidden ">
-      <div className="flex h-full">
-        {slides.map((slide, index) => (
-          <div
-            key={index}
-            className={`flex-shrink-0 h-full w-full transition duration-500 ease-in-out transform ${
-              index === currentSlide ? 'block' : 'hidden'
-            }`}
-          >
-            {/* <p>
-              {slide.info}
-            </p> */}
-            <img
-              className="h-full w-screen object-cover"
-              src={slide.image}
-              alt={slide.caption}
-            />
-          </div>
-        ))}
+    <div className="flex flex-row overflow-x-scroll snap-x snap-mandatory">
+      <div className="flex-none max-h-screen flex-shrink-0 snap-start">
+        <img className="object-cover w-screen h-full" src="/src/assets/jmm_background.jpg" />
       </div>
-      <button
-        className="absolute top-1/2 left-0 transform -translate-y-1/2 z-10 bg-gray-900 bg-opacity-50 hover:bg-opacity-75 text-white font-bold py-2 px-4 rounded-l"
-        onClick={handlePrev}
-      >
-        Previous
-      </button>
-      <button
-        className="absolute top-1/2 right-0 transform -translate-y-1/2 z-10 bg-gray-900 bg-opacity-50 hover:bg-opacity-75 text-white font-bold py-2 px-4 rounded-r"
-        onClick={handleNext}
-      >
-        Next
-      </button>
+      <div className="flex-none max-h-screen flex-shrink-0 snap-start">
+        <img className="object-cover w-screen h-full" src="/src/assets/jmm_group.jpg" />
+      </div>
+      <div className="flex-none max-h-screen flex-shrink-0 snap-start">
+        <img className="object-cover w-screen h-full" src="/src/assets/self.jpg" />
+      </div>
     </div>
+
+
+    // <div className="relative w-full overflow-hidden ">
+    //   <div className="flex h-full">
+    //     {slides.map((slide, index) => (
+    //       <div
+    //         key={index}
+    //         className={`flex-shrink-0 h-full w-full transition duration-500 ease-in-out transform ${
+    //           index === currentSlide ? 'block' : 'hidden'
+    //         }`}
+    //       >
+    //         {/* <p>
+    //           {slide.info}
+    //         </p> */}
+    //         <img
+    //           className="h-full w-screen object-cover"
+    //           src={slide.image}
+    //           alt={slide.caption}
+    //         />
+    //       </div>
+    //     ))}
+    //   </div>
+    //   <button
+    //     className="absolute top-1/2 left-0 transform -translate-y-1/2 z-10 bg-gray-900 bg-opacity-50 hover:bg-opacity-75 text-white font-bold py-2 px-4 rounded-l"
+    //     onClick={handlePrev}
+    //   >
+    //     Previous
+    //   </button>
+    //   <button
+    //     className="absolute top-1/2 right-0 transform -translate-y-1/2 z-10 bg-gray-900 bg-opacity-50 hover:bg-opacity-75 text-white font-bold py-2 px-4 rounded-r"
+    //     onClick={handleNext}
+    //   >
+    //     Next
+    //   </button>
+    // </div>
   );
 }
